685394631d91ce78951ac32762b01616b7431434
Devin Pastoor <devin.pastoor@gmail.com>
2021-03-14 10:01:20 -0400

    Create base-setup.sh
    simplified base setup that drops some TS specific needs

---
+++ base-setup.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+set -e
+
+# helper function for installing a package via brew
+brew_pkg(){
+    brew install $1
+}
+
+# helper function for installing a package via brew cask
+brew_cask(){
+    brew cask install $1
+}
+
+# helper function for installing a package via brew tap
+brew_tap(){
+    local TAP=$1
+    local PKG=$2
+    brew tap $TAP
+    brew_pkg $PKG
+}
+
+# Temporarily disable gatekeeper to prevent installations from failing
+sudo spctl --master-disable
+
+# Install homebrew if it's not already installed
+type brew &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install packages
+BREW_PKGS=(
+  coreutils
+  bash
+  bash-git-prompt
+  bash-completion
+  binutils
+  diffutils
+  ed
+  findutils
+  gawk
+  gnu-indent
+  gnu-sed
+  gnu-tar
+  gnu-which
+  gnutls
+  grep
+  gzip
+  screen
+  watch
+  wdiff
+  wget
+  go
+  python3
+  git
+  awscli
+  jq
+  ripgrep
+  hyperfine
+  fd
+)
+for pkg in ${BREW_PKGS[@]};do brew_pkg $pkg;done
+
+# Install casks
+BREW_CASKS=(
+  visual-studio-code
+  docker
+)
+for cask in ${BREW_CASKS[@]};do brew_cask $cask;done
+
+# Install taps
+# FORMAT: <tap_url_or_github_repo>,<pkg_name>
+BREW_TAPS=(
+  metrumresearchgroup/homebrew-tap,pkgr
+)
+for tap in ${BREW_TAPS[@]};do
+  TAP=$(echo $tap | cut -d, -f1)
+  PKG=$(echo $tap | cut -d, -f2)
+  brew_tap $TAP $PKG
+done
+
+# Make sure you get all the PATH elements into ~/.bash_profile.
+# You may still need to manually tweak things to your liking.
+echo 'PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/binutils/bin:/usr/local/opt/gettext/bin:/usr/local/opt/gnu-getopt/bin:/usr/local/opt/ed/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-indent/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/opt/gnu-which/libexec/gnubin:/usr/local/opt/grep/libexec/gnubin:${HOME}/Library/Python/3.7/bin:$PATH"' >> ~/.bash_profile_path_gnu
+
+# Add our shiny new shell to the list of approved shells in macOS
+grep -q "/usr/local/bin/bash" /etc/shells || echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
+
+# Switch to our new shell permanently
+chsh -s /usr/local/bin/bash
+
+# I'm too good to constantly type my password ...
+ME=$(whoami)
+echo "$ME   ALL = (ALL) NOPASSWD:ALL" | sudo tee -a /private/etc/sudoers.d/$ME
+sudo chmod 0400 /private/etc/sudoers.d/$ME
+
+# Re-enable gatekeeper
+sudo spctl --master-enable
+sudo spctl --add /Applications/Visual\ Studio\ Code.app
+
+# Add git bash-completion to shell profile
+echo '[[ -r "/usr/local/etc/bash_completion.d/git-completion.bash" ]] && . "/usr/local/etc/bash_completion.d/git-completion.bash"' >> ~/.bash_profile
+
+# Add fancy bash prompt when in git repos
+cat <<GIT >> ~/.bash_profile
+if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+  GIT_PROMPT_ONLY_IN_REPO=1
+  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
+GIT
+
+# Turn off scrollbars in Terminal.app
+defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
